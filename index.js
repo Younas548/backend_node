1593c71e939857d92d2a49f9ce7f5987f42555b4
Muhammad Ali <aliyounas343250@gmail.com>
2025-08-07 23:52:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+require(`dotenv`).config()
+const express =require(`express`);
+const app= express()
+const port =3000
+
+app.get(`/` , (req , res) => {
+    res.send(`hello ali i am here`)
+})
+
+app.get(`/login`, (req,res) => {
+    res.send ("please login me ")
+})
+
+app.get(`/signup`,(req,res) => {
+    res.send("please signup me")
+})
+
+app.listen(process.env.PORT ,() => {
+    console.log(`App listen on port ${port}`);
+    
+})
